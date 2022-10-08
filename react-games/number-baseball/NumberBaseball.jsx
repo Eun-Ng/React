cc4f66a849863303800b2e4ea7fcc5be67a7041e
Eun-Ng <dhkdldms@gmail.com>
2022-10-08 22:02:10 +0900

    Add: NumberBaseball

--- react-games/number-baseball/NumberBaseball.jsx
+++ react-games/number-baseball/NumberBaseball.jsx
@@ -1,6 +1,45 @@
 import React, {useRef, useState, useCallback} from 'react';
 import Try from './Try';
 
-const NumberBaseball = () => {};
+const getNumbers = () => {
+  // 숫자 네개를 랜덤하게 뽑는 함수
+};
+
+const NumberBaseball = () => {
+  const [answer, setAnswer] = useState(getNumbers);
+  const [result, setResult] = useState('');
+  const [value, setValue] = useState('');
+  const [tries, setTries] = useState([]);
+  const inputEl = useRef(null);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+  };
+
+  const onChangeInput = () => {};
+
+  return (
+    <>
+      <h1>{result}</h1>
+      <form onSubmit={onSubmitFrom}>
+        <input
+          ref={inputEl}
+          maxLength={4}
+          value={value}
+          onChange={onChangeInput}
+        />
+        <button>입력</button>
+        <div>
+          기회: {tries.length}/{10}
+        </div>
+        <ul>
+          {tries.map((a, b) => (
+            <Try />
+          ))}
+        </ul>
+      </form>
+    </>
+  );
+};
 
 export default NumberBaseball;
